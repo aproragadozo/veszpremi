d1a7f2839f046068bdca3cd3c6afac9730e8bd04
aproragadozo <aproragadozo@gmail.com>
2018-08-18 11:33:06 +0200

    press page extended

--- app/components/Press.jsx
+++ app/components/Press.jsx
@@ -37,29 +37,114 @@ class Press extends React.Component{
 		this.state = {
 			cards: [
 				{
-					text: 'PS Magazin',
-					link: "http://psmagazin.hu/cikk/magyar_divattervezok_a_ruhakon_tul/",
+					text: 'We Heart',
+					link: "https://www.we-heart.com/2018/05/14/design-award-2018-competition-winners/",
 					image: require('../img/press/01.jpg')
 				},
 				{
-					text: 'Styledit',
-					link: "https://www.styledit.hu/igeny-van-mar-a-magyar-designer-cipokre-is-veszpremi-gabival-beszelgettunk-48946",
+					text: 'Not Just A Label',
+					link: "https://www.notjustalabel.com/designer/gabriellaveszpremi",
 					image: require('../img/press/02.jpg')
 				},
 				{
 					text: "Rebel Live Magazine",
-					link: "https://rebellivemagazine.com/2018/05/12/amikor-a-divat-az-utcai-muveszet-az-irodalom-es-a-zene-talalkozik-urbsart-2018-roma/",
+					link: "https://rebellivemagazine.com/2018/06/14/magyar-divat-es-formatervezok-fenntarthato-alkotasai-a-helsinki-design-weeken/",
 					image: require('../img/press/03.jpg')
+				},
+				{
+					text: "PS Magazin",
+					link: "http://psmagazin.hu/cikk/magyar_divattervezok_a_ruhakon_tul/",
+					image: require('../img/press/04.jpg')
+				},
+				{
+					text: "Styledit",
+					link: "https://www.styledit.hu/igeny-van-mar-a-magyar-designer-cipokre-is-veszpremi-gabival-beszelgettunk-48946",
+					image: require('../img/press/05.jpg')
+				},
+				{
+					text: "Rebel Live Magazine",
+					link: "https://rebellivemagazine.com/2018/05/12/amikor-a-divat-az-utcai-muveszet-az-irodalom-es-a-zene-talalkozik-urbsart-2018-roma/",
+					image: require('../img/press/06.jpg')
 				},
 				{
 					text: "Marie Claire",
 					link: "http://marieclaire.hu/divat/2018/04/29/v4-divattervezok-mutatkoznak-be-romaban/",
-					image: require('../img/press/04.jpg')
+					image: require('../img/press/07.jpg')
 				},
 				{
 					text: "Petőfi Live",
 					link: "https://www.petofilive.hu/barbiblogja/cikk/2018/04/20/hazai-kollekcio-a-fenntarthato-divat-jegyeben/",
-					image: require('../img/press/05.jpg')
+					image: require('../img/press/08.jpg')
+				},
+				{
+					text: 'Bence Csalár',
+					link: "http://bencecsalar.com/gabriellaveszpremi-innovativ-retegek/",
+					image: require('../img/press/09.jpg')
+				},
+				{
+					text: "Rebel Live Magazine",
+					link: "https://rebellivemagazine.com/2017/09/28/a-bor-szimbolikus-jelentesenek-valtozasa-veszpremi-gabival-beszelgettunk/",
+					image: require('../img/press/10.jpg')
+				},
+				{
+					text: "Marie Claire",
+					link: "https://marieclaire.hu/divat/2017/08/30/fenntarthato-cipocsodak-magyar-tervezotol/",
+					image: require('../img/press/11.jpg')
+				},
+				{
+					text: "Designisso",
+					link: "http://designisso.com/hu/2017/07/07/tokeletesen-felhasznalt-alapanyag-veszpremi-gabriella-cipo-kollekciojaban/",
+					image: require('../img/press/12.jpg')
+				},
+				{
+					text: "Marie Claire",
+					link: "https://marieclaire.hu/divat/2017/07/31/rajtuk-all-a-magyar-divat-sorsa-a-legigeretesebb-frissen-vegzett-divattervezok/",
+					image: require('../img/press/13.jpg')
+				},
+				{
+					text: "MOME",
+					link: "http://diploma.mome.hu/2017/ma/veszpremi-gabriella-maria",
+					image: require('../img/press/14.jpg')
+				},
+				{
+					text: "Behance",
+					link: "https://www.behance.net/gallery/54882413/GABRIELLAVESZPREMI-Identity",
+					image: require('../img/press/15.jpg')
+				},
+				{
+					text: "Marie Claire",
+					link: "https://marieclaire.hu/lookbook/2017/05/18/gabriellaveszpremi-2017-tavaszi-nyari-taskakollekcio/",
+					image: require('../img/press/16.jpg')
+				},
+				{
+					text: "Marie Claire",
+					link: "https://marieclaire.hu/divat/2017/04/09/bemutatjuk-a-gabriellaveszpremi-imadnivalo-taskait/",
+					image: require('../img/press/17.jpg')
+				},
+				{
+					text: "We Love Budapest",
+					link: "https://welovebudapest.com/toplistak/taskak-tartalommal-kedvenc-shoppereink-budapesti-muhelyekbol/",
+					image: require('../img/press/18.jpg')
+				},
+				{
+					text: "Meyer Eszter Virág",
+					link: "http://www.meyeresztervirag.com/2016/12/a-noi-taska-titkos-univerzumaban.html",
+					image: require('../img/press/19.jpg')
+				},
+				{
+					text: "Meyer Eszter Virág",
+					link: "http://www.meyeresztervirag.com/2016/12/meyer-x-soho.html",
+					image: require('../img/press/20.jpg')
+				},
+				{
+					text: "Hestyle",
+					link: "http://hestyle.blog.hu/2014/11/11/tetszik_nem_tetszik_je_suis_belle_by_komod_taskakollekcio",
+					image: require('../img/press/21.jpg')
+				},
+				{
+					text: "Hype and Hyper",
+					link: "http://hypeandhyper.com/je-suis-belle-by-komod-aw1415/",
+					image: require('../img/press/22.jpg')
 				}
 			],
 			direction: "",
